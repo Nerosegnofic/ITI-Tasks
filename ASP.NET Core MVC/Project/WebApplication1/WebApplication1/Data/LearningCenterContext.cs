196040a592b6fa49637b64d4de5ea341629e86c2
Ahmed Abdelnabi <77232129+Nerosegnofic@users.noreply.github.com>
2025-10-01 12:33:30 +0300

    Add files via upload

--- ASP.NET Core MVC/Project/WebApplication1/WebApplication1/Data/LearningCenterContext.cs
+++ ASP.NET Core MVC/Project/WebApplication1/WebApplication1/Data/LearningCenterContext.cs
@@ -3,8 +3,10 @@ using WebApplication1.Models;
 
 namespace WebApplication1.Data
 {
-    public class LearningCenterContext(DbContextOptions<LearningCenterContext> options) : DbContext(options)
+    public class LearningCenterContext : DbContext
     {
+        public LearningCenterContext(DbContextOptions<LearningCenterContext> options) : base(options) { }
+
         public DbSet<Department> Departments { get; set; }
         public DbSet<Course> Courses { get; set; }
         public DbSet<Student> Students { get; set; }
@@ -15,39 +17,104 @@ namespace WebApplication1.Data
         {
             base.OnModelCreating(modelBuilder);
 
-            modelBuilder.Entity<CourseStudent>()
-                .HasOne(cs => cs.Course)
-                .WithMany(c => c.CourseStudents)
-                .HasForeignKey(cs => cs.CrsId)
-                .OnDelete(DeleteBehavior.Cascade);
+            // Department
+            modelBuilder.Entity<Department>(entity =>
+            {
+                entity.Property(d => d.Name)
+                      .IsRequired()
+                      .HasMaxLength(100);
 
-            modelBuilder.Entity<CourseStudent>()
-                .HasOne(cs => cs.Student)
-                .WithMany(s => s.CourseStudents)
-                .HasForeignKey(cs => cs.StdId)
-                .OnDelete(DeleteBehavior.NoAction);
+                entity.Property(d => d.ManagerName)
+                      .HasMaxLength(100);
+            });
 
-            modelBuilder.Entity<Instructor>()
-                .HasOne(i => i.Course)
-                .WithMany(c => c.Instructors)
-                .HasForeignKey(i => i.CrsId)
-                .OnDelete(DeleteBehavior.Cascade);
+            // Course
+            modelBuilder.Entity<Course>(entity =>
+            {
+                entity.Property(c => c.Name)
+                      .IsRequired()
+                      .HasMaxLength(100);
 
-            modelBuilder.Entity<Instructor>()
-                .HasOne(i => i.Department)
-                .WithMany(d => d.Instructors)
-                .HasForeignKey(i => i.DeptId)
-                .OnDelete(DeleteBehavior.NoAction);
+                entity.Property(c => c.Degree)
+                      .HasColumnType("int");
 
-            modelBuilder.Entity<Student>()
-                .HasOne(s => s.Department)
-                .WithMany(d => d.Students)
-                .HasForeignKey(s => s.DeptId);
+                entity.Property(c => c.MinimumDegree)
+                      .HasColumnType("int");
 
-            modelBuilder.Entity<Course>()
-                .HasOne(c => c.Department)
-                .WithMany(d => d.Courses)
-                .HasForeignKey(c => c.DeptId);
+                entity.Property(c => c.Hours)
+                      .HasColumnType("int");
+
+                entity.HasOne(c => c.Department)
+                      .WithMany(d => d.Courses)
+                      .HasForeignKey(c => c.DeptId)
+                      .OnDelete(DeleteBehavior.Cascade);
+            });
+
+            // Student
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.Property(s => s.Name)
+                      .IsRequired()
+                      .HasMaxLength(100);
+
+                entity.Property(s => s.Image)
+                      .HasMaxLength(255);
+
+                entity.Property(s => s.Address)
+                      .HasMaxLength(200);
+
+                entity.Property(s => s.Grade)
+                      .HasColumnType("int");
+
+                entity.HasOne(s => s.Department)
+                      .WithMany(d => d.Students)
+                      .HasForeignKey(s => s.DeptId)
+                      .OnDelete(DeleteBehavior.Cascade);
+            });
+
+            // Instructor
+            modelBuilder.Entity<Instructor>(entity =>
+            {
+                entity.Property(i => i.Name)
+                      .IsRequired()
+                      .HasMaxLength(100);
+
+                entity.Property(i => i.Salary)
+                      .HasColumnType("decimal(18,2)");
+
+                entity.Property(i => i.Address)
+                      .HasMaxLength(200);
+
+                entity.Property(i => i.Image)
+                      .HasMaxLength(255);
+
+                entity.HasOne(i => i.Department)
+                      .WithMany(d => d.Instructors)
+                      .HasForeignKey(i => i.DeptId)
+                      .OnDelete(DeleteBehavior.NoAction);
+
+                entity.HasOne(i => i.Course)
+                      .WithMany(c => c.Instructors)
+                      .HasForeignKey(i => i.CrsId)
+                      .OnDelete(DeleteBehavior.Cascade);
+            });
+
+            // CourseStudent
+            modelBuilder.Entity<CourseStudent>(entity =>
+            {
+                entity.Property(cs => cs.Degree)
+                      .HasColumnType("int");
+
+                entity.HasOne(cs => cs.Course)
+                      .WithMany(c => c.CourseStudents)
+                      .HasForeignKey(cs => cs.CrsId)
+                      .OnDelete(DeleteBehavior.Cascade);
+
+                entity.HasOne(cs => cs.Student)
+                      .WithMany(s => s.CourseStudents)
+                      .HasForeignKey(cs => cs.StdId)
+                      .OnDelete(DeleteBehavior.NoAction);
+            });
         }
     }
 }
--- ASP.NET Core MVC/Project/WebApplication1/WebApplication1/Models/Tables.cs
+++ ASP.NET Core MVC/Project/WebApplication1/WebApplication1/Models/Tables.cs
@@ -1,9 +1,17 @@
-﻿namespace WebApplication1.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication1.Models
 {
     public class Department
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public required string Name { get; set; }
+
+        [StringLength(100)]
         public string? ManagerName { get; set; }
 
         public ICollection<Course> Courses { get; set; } = [];
@@ -14,14 +22,24 @@
     public class Course
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public required string Name { get; set; }
+
+        [Range(0, 1000)]
         public int? Degree { get; set; }
+
+        [Range(0, 1000)]
         public int? MinimumDegree { get; set; }
+
+        [Range(1, 10)]
         public int? Hours { get; set; }
 
+        [Required]
         public int DeptId { get; set; }
         public required Department Department { get; set; }
-        
+
         public ICollection<CourseStudent> CourseStudents { get; set; } = [];
         public ICollection<Instructor> Instructors { get; set; } = [];
     }
@@ -29,11 +47,21 @@
     public class Student
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public required string Name { get; set; }
+
+        [StringLength(255)]
         public string? Image { get; set; }
+
+        [StringLength(200)]
         public string? Address { get; set; }
+
+        [Range(0, 100)]
         public int? Grade { get; set; }
 
+        [Required]
         public int DeptId { get; set; }
         public required Department Department { get; set; }
 
@@ -43,14 +71,25 @@
     public class Instructor
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public required string Name { get; set; }
+
+        [Range(0, 100000)]
         public decimal? Salary { get; set; }
+
+        [StringLength(200)]
         public string? Address { get; set; }
+
+        [StringLength(255)]
         public string? Image { get; set; }
 
+        [Required]
         public int DeptId { get; set; }
         public required Department Department { get; set; }
 
+        [Required]
         public int CrsId { get; set; }
         public required Course Course { get; set; }
     }
@@ -58,11 +97,15 @@
     public class CourseStudent
     {
         public int Id { get; set; }
+
+        [Range(0, 100)]
         public int? Degree { get; set; }
 
+        [Required]
         public int CrsId { get; set; }
         public required Course Course { get; set; }
 
+        [Required]
         public int StdId { get; set; }
         public required Student Student { get; set; }
     }
