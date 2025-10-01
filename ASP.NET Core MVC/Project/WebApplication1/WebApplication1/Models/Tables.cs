89c403c79763bfb3e484fce7f3f25f6e9012b657
Ahmed Abdelnabi <77232129+Nerosegnofic@users.noreply.github.com>
2025-10-01 10:39:31 +0300

    Update Tables.cs

--- ASP.NET Core MVC/Project/WebApplication1/WebApplication1/Models/Tables.cs
+++ ASP.NET Core MVC/Project/WebApplication1/WebApplication1/Models/Tables.cs
@@ -1,4 +1,4 @@
-﻿namespace WebApplication1.Models
+namespace WebApplication1.Models
 {
     public class Department
     {
@@ -6,9 +6,9 @@
         public required string Name { get; set; }
         public string? ManagerName { get; set; }
 
-        public ICollection<Course> Courses { get; set; } = new List<Course>();
-        public ICollection<Student> Students { get; set; } = new List<Student>();
-        public ICollection<Instructor> Instructors { get; set; } = new List<Instructor>();
+        public ICollection<Course> Courses { get; set; } = [];
+        public ICollection<Student> Students { get; set; } = [];
+        public ICollection<Instructor> Instructors { get; set; } = [];
     }
 
     public class Course
